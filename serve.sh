42b0ccbf40072a0f4c1c120301ce6677f345477e
marcmrf <marc.garcia@marfeel.com>
2015-12-11 00:26:08 +0100

    remove deprecated slides output

--- serve.sh
+++ serve.sh
@@ -12,11 +12,4 @@ then
   port=8000
 fi
 
-if [ $(uname -s) == "Darwin" ]
-then
-  open=open
-else
-  open=xdg-open
-fi
-
-$open http://localhost:$port/template.html && python -m SimpleHTTPServer $port;
+python -m SimpleHTTPServer $port;
